75fdb9a4ce1c28f52e2e51ca83487db74da6866f
Mikaelia <mikaelagurney@gmail.com>
2018-06-08 20:34:40 +0000

    Change parameters to kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 '''creation of BaseModel class'''
 import uuid
-import datetime
+from  datetime import datetime
 
 class BaseModel():
     '''BaseModel class'''
-    def __init__(self, id=None, created_at=None, updated_at=None):
+    def __init__(self, *args, **kwargs):
         '''Instantiation of BaseModel class
 
             Args:
@@ -15,19 +15,24 @@ class BaseModel():
             Returns:
                 Instance of BaseModel class
         '''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if (kwargs):
+                self.__dict__ = kwargs
+                self.created_at = datetime.strptime(self.created_at, '%Y-%m-%dT%H:%M:%S.%f')
+                self.updated_at = datetime.strptime(self.updated_at, '%Y-%m-%dT%H:%M:%S.%f')
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
     def __str__(self):
         '''overloads __str__ method'''
-        return("[{}] ({}) {}".format(self.__class__, self.id, self.__dict__))
+        return("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
     def save(self):
         '''updates `updated_at` with the current datetime'''
         self.updated_at = datetime.datetime.now()
     def to_dict(self):
         '''returns dictionary of the instance'''
-        self.__dict__['__class__'] = self.__class__.__name__
-        self.created_at = self.created_at.isoformat()
-        self.updated_at = self.updated_at.isoformat()
-        return self.__dict__
-
+        mydict = dict(vars(self))
+        mydict['__class__'] = self.__class__.__name__
+        mydict['created_at'] = self.created_at.isoformat()
+        mydict['updated_at'] = self.updated_at.isoformat()
+        return mydict
