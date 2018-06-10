c1ddc306d7d403035ef6480b2b2b1839ffdfbf8b
Mikaelia <mikaelagurney@gmail.com>
2018-06-10 16:51:38 +0000

    Add show method and modularize code
    I added the show class method, but also noticed that I would have to repeat
    the argument checks frequently, so I put the process in another
    function that returns the object dictionary and key value of model
    might break this apart too, later

--- console.py
+++ console.py
@@ -1,25 +1,54 @@
 #!/usr/bin/python3
 import cmd
 from models.base_model import BaseModel
+from models import storage
 
+classes = ['BaseModel', 'User', 'State',
+           'City', 'Amenity', 'Place', 'Review']
 
-classes = ['BaseModel']
+def checkme(args):
+    arglist = args.split(' ')
+    argcount = len(arglist)
+    objdict = {}
+
+    if args is '':
+        print('** class name missing **')
+    elif argcount < 2:
+        print('** instance id missing **')
+    elif arglist[0] not in classes:
+        print("** class doesn't exist **")
+
+    else:
+        objdict = storage.all()
+        key = '{}.{}'.format(arglist[0], arglist[1])
+        if key in objdict.keys():
+            return (objdict, key)
+        else:
+            print('** no instance found **')
+
 
 class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         if not args:
             print('** class name missing **')
-        if args not in classes:
+            return
+        elif args not in classes:
             print("** class doesn't exist **")
+            return
         else:
             new = BaseModel()
             new.save()
             print(new.id)
 
+    def do_show(self, args=''):
+        '''Prints the string representation of an instance'''
+        objtuple = checkme(args)
+        if objtuple:
+            print(objtuple[0][objtuple[1]])
 
     def do_quit(self, args):
-        """Quits the program."""
+        '''Quits the program.'''
         raise SystemExit
 
     def emptyline(self):
@@ -27,6 +56,7 @@ class HBNBCommand(cmd.Cmd):
 
     def help_quit(self):
         print('Quit command to exit the program\n')
+
 if __name__ == '__main__':
     prompt = HBNBCommand()
     prompt.prompt = '(hbnb) '
