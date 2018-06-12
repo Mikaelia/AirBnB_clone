80d245b79f41550a5b05a6a9be33faddb5244d6b
Mikaelia <mikaelagurney@gmail.com>
2018-06-12 23:39:41 +0000

    Minor syntax change from FileStorage to self

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,30 +16,30 @@ class FileStorage():
     __objects = {}
 
     def all(self):
-        '''Returns the dictionary __objects'''
-        return FileStorage.__objects
+        '''Returns the dictionary {<class_name>.<id>:<class_instance>}'''
+        return self.__objects
 
     def new(self, obj):
-        '''Sets class.id key with obj value'''
-        FileStorage.__objects["{}.{}".format(
-            obj.__class__.__name__, obj.id)] = obj
+        '''Formats key and assings corresponding object value'''
+        if obj:
+            self.__objects["{}.{}".format(
+                obj.__class__.__name__, obj.id)] = obj
 
     def save(self):
-        '''Serializes __objects to the JSON file'''
+        '''Serializes __objects dict and stores in JSON file'''
         mydict = {}
-        for k, v in FileStorage.__objects.items():
+        for k, v in self.__objects.items():
             mydict[k] = v.to_dict()
-        with open(FileStorage.__file_path, mode='w', encoding='utf-8') as f:
+        with open(FileStorage.__file_path, mode='w+') as f:
             json.dump(mydict, f)
 
     def reload(self):
-        '''Deserializes the JSON file to __objects if file exists'''
+        '''Deserializes JSON file to dict with obj instances if file exists'''
         try:
-            with open(FileStorage.__file_path,
-                      mode='r', encoding='utf-8') as f:
+            with open(self.__file_path, mode='r') as f:
                 jdict = json.load(f)
             for k, v in jdict.items():
                 newobj = eval(v['__class__'])(**v)
-                FileStorage.__objects[k] = newobj
+                self.__objects[k] = newobj
         except FileNotFoundError:
             pass
