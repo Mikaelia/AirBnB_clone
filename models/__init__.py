edc8142fe32627f4790845588589401c0de5c253
Mikaelia <mikaelagurney@gmail.com>
2018-06-24 00:22:38 +0000

    remove all files

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-'''Init file'''
-from models.engine.file_storage import FileStorage
-
-
-storage = FileStorage()
-storage.reload()
--- models/state.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-'''State class module'''
-from models.base_model import BaseModel
-
-
-class State(BaseModel):
-    '''State class'''
-    name = ''
